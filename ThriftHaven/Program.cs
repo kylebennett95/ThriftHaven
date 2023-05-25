d2ac1afa002e83cf38b37a2d780bebd17ed7af41
Kyle <kylebennett9514@gmail.com>
2023-05-25 18:13:39 -0500

    Implemented addTransient in program.cs, fixed user secrets, tested functionality.

--- ThriftHaven/Program.cs
+++ ThriftHaven/Program.cs
@@ -1,3 +1,4 @@
+using ThriftHaven.Repositories;
 using FirebaseAdmin;
 using Google.Apis.Auth.OAuth2;
 using Microsoft.AspNetCore.Builder.Extensions;
@@ -7,6 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
+builder.Services.AddTransient<IFavoriteRepository, FavoriteRepository>();
+builder.Services.AddTransient<IListingRepository, ListingRepository>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
