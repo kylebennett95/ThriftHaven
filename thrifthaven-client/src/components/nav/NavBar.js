917b489688c2b16c79352d46aa70d5d6bd8fb364
Kyle <kylebennett9514@gmail.com>
2023-06-05 21:39:02 -0500

    Got home page working, added create listing button and page

--- thrifthaven-client/src/components/nav/NavBar.js
+++ thrifthaven-client/src/components/nav/NavBar.js
@@ -19,7 +19,7 @@ export const NavBar = () => {
 
 
 
-<nav class="bg-yellow-700 border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+<nav class="bg-yellow-400 border-gray-200 dark:bg-gray-900 dark:border-gray-700">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="/" class="flex items-center">
         <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
@@ -70,7 +70,7 @@ export const NavBar = () => {
           <a href="favorites" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Favorites</a>
         </li>
         <li>
-          <a href="listing" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">My Listings</a>
+          <a href="mylistings" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">My Listings</a>
         </li>
         <li>
           <a href="profile" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Profile</a>
