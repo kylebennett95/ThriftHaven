9f4684f0b715364e7593d001a975f234309dd7fb
Kyle <kylebennett9514@gmail.com>
2023-06-13 23:51:46 -0500

    Got the logout button working, got favorites data to render on the favorites listing.

--- thrifthaven-client/src/components/ThriftHaven.js
+++ thrifthaven-client/src/components/ThriftHaven.js
@@ -14,7 +14,6 @@ export const ThriftHaven = () => {
         element={
           <Authorized>
             <>
-                
                 <ApplicationViews />
             </>
           </Authorized>
